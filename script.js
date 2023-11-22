bc1fcfe8cdc68b2f78734ae27fb3f2b2731e5b92
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2023-11-22 09:21:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If required.
-function functionName() {
-	// alert("functionName");
-	alert(arguments.callee.name);
-}
-functionName();
+// function functionName() {
+// 	alert("functionName");
+// }
+// functionName();
 function AccioJob() {
 	alert("AccioJob")
 }
